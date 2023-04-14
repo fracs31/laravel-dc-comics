802cf02b9092b430ceafcbfbbc76639ceb8d1614
fracs31 <pist.fracs95@gmail.com>
2023-04-14 15:10:03 +0200

    ADD: layout and index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,42 @@
+{{-- Layout --}}
+@extends('layouts.app')
+{{-- Contenuto --}}
+@section('content')
+    {{-- Container --}}
+    <div class="container">
+        {{-- Titolo --}}
+        <h1>
+            Tutti i fumetti
+        </h1>
+        {{-- Tabella --}}
+        <table class="table">
+            {{-- Testa --}}
+            <thead>
+              <tr>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Copertina</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data di uscita</th>
+                <th scope="col">Tipo</th>
+              </tr>
+            </thead>
+            {{-- Corpo --}}
+            <tbody>
+                {{-- Ciclo --}}
+                @foreach ($comics as $comic)
+                    <tr>
+                        <td>{{ $comic["title"] }}</td>
+                        <td>{{ $comic["description"] }}</td>
+                        <td>{{ $comic["thumb"] }}</td>
+                        <td>{{ $comic["price"] }}</td>
+                        <td>{{ $comic["series"] }}</td>
+                        <td>{{ $comic["sale_date"] }}</td>
+                        <td>{{ $comic["type"] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController; //controller
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Index
+Route::get('/', [ComicController::class, "index"])->name("comics.index");
