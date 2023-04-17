e3fa2645ea83d013b896ae940d9ff07629776a91
fracs31 <pist.fracs95@gmail.com>
2023-04-17 15:48:24 +0200

    CHANGE: table layout in index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -14,9 +14,7 @@
             <thead>
               <tr>
                 <th scope="col">Titolo</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Copertina</th>
-                <th scope="col">Prezzo</th>
+                <th scope="col">Prezzo ($)</th>
                 <th scope="col">Serie</th>
                 <th scope="col">Data di uscita</th>
                 <th scope="col">Tipo</th>
@@ -29,13 +27,14 @@
                 @foreach ($comics as $comic)
                     <tr>
                         <td>{{ $comic->title }}</td>
-                        <td>{{ $comic->description }}</td>
-                        <td>{{ $comic->thumb }}</td>
                         <td>{{ $comic->price }}</td>
                         <td>{{ $comic->series }}</td>
                         <td>{{ $comic->sale_date }}</td>
                         <td>{{ $comic->type }}</td>
-                        <td><a href="{{ route("comics.show", $comic->id)}}">Info</a></td>
+                        <td>
+                            <a class="btn btn-primary" href="{{ route("comics.show", $comic)}}">Info</a>
+                            <a class="btn btn-warning" href="{{ route("comics.edit", $comic)}}">Edita</a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
